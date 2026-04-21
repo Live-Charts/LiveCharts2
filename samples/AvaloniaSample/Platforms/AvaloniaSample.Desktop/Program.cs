36642c88c9be7c571eca9f953a92dee58a11dd81
beto-rodriguez <rodriguezo.alberto@gmail.com>
2026-04-21 13:19:35 -0600

    chore: remove reactiveui from avalonia sample

--- samples/AvaloniaSample/Platforms/AvaloniaSample.Desktop/Program.cs
+++ samples/AvaloniaSample/Platforms/AvaloniaSample.Desktop/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 
 using Avalonia;
-using Avalonia.ReactiveUI;
 
 namespace AvaloniaApplication1.Desktop;
 
@@ -19,6 +18,5 @@ class Program
         => AppBuilder.Configure<AvaloniaSample.App>()
             .UsePlatformDetect()
             .WithInterFont()
-            .LogToTrace()
-            .UseReactiveUI();
+            .LogToTrace();
 }
